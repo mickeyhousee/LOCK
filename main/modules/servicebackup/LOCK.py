defd4e67079d4865ff4f944215df43a1f05826a6
João Grilo <joaogrilo2222@gmail.com>
2024-04-08 09:25:05 +0100

    Changes in LOCK.py

--- main/modules/servicebackup/LOCK.py
+++ main/modules/servicebackup/LOCK.py
@@ -1,8 +1,14 @@
+#!/usr/bin/env python3
+"""__summary__
+Script made for LS24
+
+Author: Development Team COCIBER PT
+"""
 import hashlib
 import os
 import shutil
 import logging
-import time  # This import is necessary for time.sleep()
+import time 
 from datetime import datetime
 from logging.handlers import RotatingFileHandler
 
@@ -16,9 +22,12 @@ logging.basicConfig(
         logging.StreamHandler()
     ]
 )
+# Relative Path
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
+print(dir_path)
 # Directories and file paths (to be adjusted to your actual paths)
-MONITOR_DIR = '/home/onezero/LOCK/prototype/files'  # Directory containing files to be monitored
+MONITOR_DIR = f'{dir_path}/monitor'  # Directory containing files to be monitored
 BACKUP_DIR = '/home/onezero/LOCK/prototype/backup'  # Directory where backups will be stored
 QUARANTINE_DIR = '/home/onezero/LOCK/prototype/quarantine'  # Directory where suspicious files will be quarantined
 HASH_FILE = 'hashes.csv'  # File where current hashes will be stored
